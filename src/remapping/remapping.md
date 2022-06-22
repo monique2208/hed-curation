a6a32c38564e5761d0a6d8f88e930984b0324053
b1071125 <monique.denissen@sbg.ac.at>
2022-06-22 16:10:07 -0700

    Update run scripts and instructions

---
+++ src/remapping/remapping.md
@@ -0,0 +1,18 @@
+Run the run.py script with following arguments:
+
+		-d bids_dir 
+		-t task_name 
+		-o operations_json_path
+
+Example:
+
+		python .\remapping\run.py -d D:\user\ds002790sm_adaptedcolumns -t task_name -o enhance_stopsignal.json
+
+Run the restore script with the following arguments:
+
+		-d bids_dir 
+		-t task_name
+
+Example:
+
+		python .\remapping\restore.py -d D:\user\ds002790sm_adaptedcolumns -t task_name
---
+++ src/remapping/run.py
@@ -0,0 +1,34 @@
+import argparse
+import pandas as pd
+from operations.operations import run_operations
+import bids
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description="""Converts event files based on a json file specifying
+        operations.""")
+    parser.add_argument(
+        "-d",
+        dest="bids_dir",
+        help="")
+    parser.add_argument(
+        "-t",
+        dest="task_name",
+        help="")
+    parser.add_argument(
+        "-o",
+        dest="operations_json_path",
+        help="")
+    args = parser.parse_args()
+
+    all_events_paths = bids.find_task_files(args.bids_dir, args.task_name)
+    operations_dict = bids.load_operations(args.operations_json_path)
+
+    for events_file_path in all_events_paths:
+        print('Loading file: %s' % events_file_path)
+        print('')
+        events = pd.read_csv(events_file_path, sep='\t')
+
+        print('Restructuring %s' % events_file_path)
+        events = run_operations(events, operations_dict)
+        bids.rename_and_save_new(events, events_file_path)
